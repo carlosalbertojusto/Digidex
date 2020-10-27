d385ae5675ba17f10cdfedb197f34a4d1bfd6a47
unknown <carlosjustodev@gmail.com>
2020-10-26 22:08:23 -0400

    first attempt

---
+++ src/components/digimons.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="digimon">
+    <h2 id="d_name">Digimons Serão listados aqui!!</h2>
+    <h3>Poder do Digimon!!</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#d_name {
+  width: 180px;
+  height: 120px;
+  border: none;
+  background: #12afcd;
+  border-radius: 1.6rem;
+  cursor: pointer;
+
+  display: flex; 
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
